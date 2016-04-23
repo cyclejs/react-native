ac7d91b8fb8a868f452ee2af12e4cfbcdf7fccfe
Jani Eväkallio <jani.evakallio@gmail.com>
2016-04-23 19:26:36 +0200

    Refactor driver for global touch event hooks

--- src/driver.js
+++ src/driver.js
@@ -2,51 +2,37 @@ import React from 'react-native'
 import Rx from 'rx'
 const {AppRegistry, View} = React
 
+let handlers = {}
+
+export function registerHandler(selector, evType) {
+  handlers[selector] = handlers[selector] || {};
+  handlers[selector][evType] = handlers[selector][evType] || new Rx.Subject();
+  handlers[selector][evType].send = function sendIntoSubject(...args) {
+    const props = this
+    const event = {currentTarget: {props}, args}
+    handlers[selector][evType].onNext(event)
+  }
+
+  return handlers[selector][evType];
+};
+
+export function findHandler(selector, evType) {
+  if (handlers[selector].hasOwnProperty(evType)) {
+    return handlers[selector][evType].send
+  }
+}
+
 function isChildReactElement(child) {
   return !!child && typeof child === `object` && child._isReactElement
 }
 
 function makeReactNativeDriver(appKey) {
   return function reactNativeDriver(vtree$) {
-    let handlers = {}
-
-    function augmentVTreeWithHandlers(vtree, index = null) {
-      if (typeof vtree === `string` || typeof vtree === `number`) {
-        return vtree
-      }
-      let newProps = {}
-      if (!vtree.props.selector && !!index) {
-        newProps.selector = index
-      }
-      let wasTouched = false
-      if (handlers[vtree.props.selector]) {
-        for (let evType in handlers[vtree.props.selector]) {
-          if (handlers[vtree.props.selector].hasOwnProperty(evType)) {
-            let handlerFnName = `on${evType.charAt(0).toUpperCase()}${evType.slice(1)}`
-            newProps[handlerFnName] = handlers[vtree.props.selector][evType].send
-            wasTouched = true
-          }
-        }
-      }
-      let children = vtree.props.children
-      if (Array.isArray(vtree.props.children)) {
-        return React.cloneElement(vtree, newProps,
-          ...children.map(augmentVTreeWithHandlers))
-      } else if (isChildReactElement(vtree.props.children)) {
-        return React.cloneElement(vtree, newProps,
-          augmentVTreeWithHandlers(children))
-      } else if (wasTouched) {
-        return React.cloneElement(vtree, newProps, children)
-      }
-      return vtree
-    }
-
     function componentFactory() {
       return React.createClass({
         componentWillMount() {
-          vtree$.subscribe(rawVTree => {
-            let replacedVTree = augmentVTreeWithHandlers(rawVTree)
-            this.setState({vtree: replacedVTree})
+          vtree$.subscribe(newVTree => {
+            this.setState({vtree: newVTree})
           })
         },
         getInitialState() {
@@ -63,14 +49,7 @@ function makeReactNativeDriver(appKey) {
         return {
           observable: Rx.Observable.empty(),
           events: function events(evType) {
-            handlers[selector] = handlers[selector] || {}
-            handlers[selector][evType] = handlers[selector][evType] || new Rx.Subject()
-            handlers[selector][evType].send = function sendIntoSubject(...args) {
-              const props = this
-              const event = {currentTarget: {props}, args}
-              handlers[selector][evType].onNext(event)
-            }
-            return handlers[selector][evType]
+            return registerHandler(selector, evType);
           },
         }
       },
