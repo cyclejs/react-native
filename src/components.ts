991f552d3dcb087b2accda0e802feff4d06df0ce
Andre Staltz <andre@staltz.com>
2021-12-13 14:45:17 +0200

    update components for react-native 0.64

--- src/components.ts
+++ src/components.ts
@@ -1,30 +1,29 @@
 import {
+  // General
   ActivityIndicator as _ActivityIndicator,
   Button as _Button,
-  DatePickerIOS as _DatePickerIOS,
-  DrawerLayoutAndroid as _DrawerLayoutAndroid,
   FlatList as _FlatList,
   Image as _Image,
-  InputAccessoryView as _InputAccessoryView,
   KeyboardAvoidingView as _KeyboardAvoidingView,
-  MaskedViewIOS as _MaskedViewIOS,
   Modal as _Modal,
-  NavigatorIOS as _NavigatorIOS,
+  Pressable as _Pressable,
   RefreshControl as _RefreshControl,
   ScrollView as _ScrollView,
   SectionList as _SectionList,
-  SnapshotViewIOS as _SnapshotViewIOS,
   StatusBar as _StatusBar,
   Switch as _Switch,
-  TabBarIOS as _TabBarIOS,
-  TextInput as _TextInput,
   Text as _Text,
-  ToolbarAndroid as _ToolbarAndroid,
+  TextInput as _TextInput,
   TouchableHighlight as _TouchableHighlight,
-  TouchableNativeFeedback as _TouchableNativeFeedback,
   TouchableOpacity as _TouchableOpacity,
   TouchableWithoutFeedback as _TouchableWithoutFeedback,
   View as _View,
+  // Android
+  DrawerLayoutAndroid as _DrawerLayoutAndroid,
+  TouchableNativeFeedback as _TouchableNativeFeedback,
+  // iOS
+  InputAccessoryView as _InputAccessoryView,
+  SafeAreaView as _SafeAreaView,
 } from 'react-native';
 import {makeHelper} from './helper';
 
@@ -35,19 +34,14 @@ export const FlatList = makeHelper(_FlatList);
 export const Image = makeHelper(_Image);
 export const InputAccessoryView = makeHelper(_InputAccessoryView);
 export const KeyboardAvoidingView = makeHelper(_KeyboardAvoidingView);
-export const MaskedViewIOS = makeHelper(_MaskedViewIOS);
 export const Modal = makeHelper(_Modal);
-export const NavigatorIOS = makeHelper(_NavigatorIOS);
 export const RefreshControl = makeHelper(_RefreshControl);
 export const ScrollView = makeHelper(_ScrollView);
 export const SectionList = makeHelper(_SectionList);
-export const SnapshotViewIOS = makeHelper(_SnapshotViewIOS);
 export const StatusBar = makeHelper(_StatusBar);
 export const Switch = makeHelper(_Switch);
-export const TabBarIOS = makeHelper(_TabBarIOS);
 export const TextInput = makeHelper(_TextInput);
 export const Text = makeHelper(_Text);
-export const ToolbarAndroid = makeHelper(_ToolbarAndroid);
 export const TouchableHighlight = makeHelper(_TouchableHighlight);
 export const TouchableNativeFeedback = makeHelper(_TouchableNativeFeedback);
 export const TouchableOpacity = makeHelper(_TouchableOpacity);
